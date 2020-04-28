98de7927a3b38c78ded2c0e20db9fb6f18a50205
Stephen Peters <7speter@gmail.com>
2020-04-28 12:29:30 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,11 @@
 def find_max_value(array)
   counter = 0
+  max_value = -1
   while array.length > 1 do
-    if array[counter] < array[counter +1]
-      array.delete_at(array, array[counter])
-      #elsif array[counter] > array[counter + 1]
-        #array
-
-      counter +=1
+    if max_value < array[count]
+      max_value = array[count]
     end
+    count += 1
   end
+  max_value
 end
